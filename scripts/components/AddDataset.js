2ef81e04083813f2303eb266ba4bc8553a803a34
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-09-04 18:04:49 -0400

    Add Dataset page cleanup
    * Push more needed props into dataset form
    * Better state management

--- scripts/components/AddDataset.js
+++ scripts/components/AddDataset.js
@@ -1,58 +1,55 @@
 import React from 'react';
 
-// firebase
-var firebaseEndpoint = 'https://metaseq-6b779.firebaseio.com/';
-var Rebase = require('re-base');
+// Firebase imports / setup
+import Rebase from 're-base';
 import Firebase from 'firebase';
-var base = Rebase.createClass(firebaseEndpoint);
-var firebaseRoot = new Firebase(firebaseEndpoint);
 
-// Component imports
+// Material Design imports
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// My component imports
 import AddDatasetForm from './AddDatasetForm';
 import Header from './Header';
 
-/*
-  AddDataset
-  AddDataset list on right side of page
-*/
+// Firebase setup
+var firebaseEndpoint = 'https://metaseq-6b779.firebaseio.com/';
+var base = Rebase.createClass(firebaseEndpoint);
+var firebaseRoot = new Firebase(firebaseEndpoint);
 
 var AddDataset = React.createClass({
     getInitialState: function() {
-        var datasetId = (new Date()).getTime();
         return {
           'dataset':{
             'owner': 'nb',
             'fields': {}
           },
-          'datasetId':datasetId
+          'datasetId':0
         }
     },
-
     componentWillMount: function() {
+        var datasetId = (new Date()).getTime();
+        this.state.datasetId = datasetId;
         this.ref = base.syncState('/datasets/' + this.state.datasetId, {
             context: this,
             state: 'dataset'
         });
+        this.setState({ 'dataset' : this.state.dataset});
     },
-
     componentWillUnmount: function() {
       base.removeBinding(this.ref);
     },
-
     addDataset : function(dataset) {
       this.state.dataset.fields = dataset;
       this.state.dataset.owner = 'nb';
       this.setState({ 'dataset' : this.state.dataset});
-      this.props.history.push('/dataset/' + this.state.datasetId);
     },
-
     render: function() {
         return (
           <div>
-            <Header/>
+            <Header history={this.props.history}/>
             <div >
-            <h2>Dataset</h2>
-            <AddDatasetForm addDataset={this.addDataset} datasetId={this.state.datasetId}/>
+              <h2>Contribute a dataset</h2>
+              <AddDatasetForm addDataset={this.addDataset} datasetId={this.state.datasetId} history={this.props.history}/>
             </div>
           </div>
         )
