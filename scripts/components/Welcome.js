0cf9ac971f5a5235feee44c8bf67d7f77acc754a
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-09-04 17:02:14 -0400

    Welcome page cleanup

--- scripts/components/Welcome.js
+++ scripts/components/Welcome.js
@@ -1,89 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-// Material Design stuff
+// Material Design imports
 import RaisedButton from 'material-ui/RaisedButton';
-import Dialog from 'material-ui/Dialog';
-import {deepOrange500} from 'material-ui/styles/colors';
-import FlatButton from 'material-ui/FlatButton';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-/*
-  Welcome
-  Let's us make <Welcome/> elements
-*/
 // My component imports
 import Header from './Header';
 
 var Welcome = React.createClass({
   getInitialState : function() {
-    return {
-        open: false,
-    }
+    return {}
   },
-
-  handleRequestClose : function() {
-    this.setState({
-      open: false,
-    });
-  },
-
-  handleTouchTap : function() {
-    this.setState({
-      open: true,
-    });
-  },
-
   render : function() {
-    var muiTheme = getMuiTheme({
-      palette: {
-        accent1Color: deepOrange500,
-      },
-    });
-
-    var standardActions = (
-      <FlatButton
-        label="Ok"
-        primary={true}
-        onTouchTap={this.handleRequestClose}
-      />
-    );
-
     var styles = {
       container: {
         textAlign: 'center',
-        paddingTop: 200,
-      },
+        paddingTop: 125,
+        width: '70%',
+        margin: '0 auto',
+        maxWidth: '420px'
+      }
     };
     return (
       <div>
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <div style={styles.container}>
-          <Dialog
-            open={this.state.open}
-            title="Super Secret Password"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
-          >
-            1-2-3-4-5
-          </Dialog>
-          <h1>Material-UI</h1>
-          <h2>example project</h2>
-          <Link to='/browse'>
-            <RaisedButton
-            label="Super Secret Password"
-            secondary={true}
-            onTouchTap={this.handleTouchTap}
-            />
-          </Link>
-        </div>
-      </MuiThemeProvider>
         <Header/>
+        <MuiThemeProvider>
+          <div style={styles.container}>
+            <h1>Material-UI</h1>
+            <Link to='/browse'>
+              <RaisedButton label="Browse Discoveries" fullWidth={true}/>
+            </Link>
+            <Link to='/explore'>
+              <RaisedButton label="Explore Datasets" fullWidth={true}/>
+            </Link>
+            <Link to='/dataset/new'>
+              <RaisedButton label="Contribute Data" fullWidth={true}/>
+            </Link>
+          </div>
+        </MuiThemeProvider>
       </div>
     )
   }
-
 });
 
 export default Welcome;
