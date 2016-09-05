3e388e25754b5f30cf5976a323f64adb71c1b334
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-09-05 01:04:20 -0400

    Add filtering to Explore view
    * Busted out into 2 new components
    * Storing rulesets in state
    * Table live-updates based on filter values

---
+++ scripts/components/ExploreFilters.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+// Material Design imports
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+var ExploreFilters = React.createClass({
+  getInitialState : function() {
+    return {
+      'database':0,
+      'env_package':0
+    }
+  },
+
+  handleChange : function(type, event, index, value) {
+    this.state[type] = value;
+    this.setState(this.state);
+
+    var rules = [];
+    // This is janky as hell and won't scale beyond 3-4 without becoming a PITA
+    if (this.state.database > 0) {
+      var dbRule = {
+        'field':'database',
+        'type':4
+      };
+      if (this.state.database == 1) {
+        dbRule.value = 'EBI';
+      } else {
+        dbRule.value = 'GenBank';
+      }
+      rules.push(dbRule);
+    }
+
+    if (this.state.env_package > 0) {
+      var envRule = {
+        'field':'env_package',
+        'type':4
+      };
+      if (this.state.env_package == 1) {
+        envRule.value = 'sediment';
+      } else {
+        envRule.value = 'soil';
+      }
+      rules.push(envRule);
+    }
+
+    this.props.applyRules(rules);
+  },
+
+  render : function() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div style={{'marginLeft':80}}>
+            <h4>Choose Database</h4>
+            <SelectField value={this.state.database} onChange={this.handleChange.bind(this,'database')}>
+              <MenuItem value={0} primaryText="All" />
+              <MenuItem value={1} primaryText="EBI" />
+              <MenuItem value={2} primaryText="GenBank" />
+            </SelectField>
+            <h4>Choose Environment Package</h4>
+            <SelectField value={this.state.env_package} onChange={this.handleChange.bind(this,'env_package')}>
+              <MenuItem value={0} primaryText="All" />
+              <MenuItem value={1} primaryText="sediment" />
+              <MenuItem value={2} primaryText="soil" />
+            </SelectField>
+          </div>
+        </MuiThemeProvider>
+      </div>
+    )
+  }
+});
+
+export default ExploreFilters;
---
+++ scripts/components/ExploreTable.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+// Material Design imports
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
+from 'material-ui/Table';
+
+// My component imports
+import Header from './Header';
+
+var ExploreTable = React.createClass({
+  getInitialState : function() {
+    return {}
+  },
+  render : function() {
+    var styles = {
+      container: {
+        textAlign: 'center',
+        paddingTop: 125,
+        width: '70%',
+        margin: '0 auto',
+        maxWidth: '420px'
+      },
+      propContainer: {
+        width: 200,
+        overflow: 'hidden',
+        margin: '20px auto 0',
+      },
+      propToggleHeader: {
+        margin: '20px auto 10px',
+      }
+    };
+    return (
+      <MuiThemeProvider>
+        <div>
+            <Table bodyStyle={{overflowX: undefined, width:'2000px' }} height='600px' fixedHeader={false} fixedFooter={false} selectable={false}>
+              <TableHeader adjustForCheckbox={false} displaySelectAll={false} enableSelectAll={false}>
+                <TableRow selectable={false}>
+                  <TableHeaderColumn tooltip="id">id</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="database">database</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="sample_id">sample_id</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="investigation_type">investigation_type</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="project_name">project_name</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="lat_lon">lat_lon</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="depth">depth</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="alt_elev">alt_elev</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="geo_loc_name">geo_loc_name</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="collection_date">collection_date</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="env_biome">env_biome</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="env_feature">env_feature</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="env_material">env_material</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="env_package">env_package</TableHeaderColumn>
+                  <TableHeaderColumn tooltip="seq_meth">seq_meth</TableHeaderColumn>
+                </TableRow>
+              </TableHeader>
+              <TableBody stripedRows={true} displayRowCheckbox={false} preScanRows={false}>
+                {this.props.activeData.map( (row, index) => (
+                  <TableRow selectable={false} key={index}>
+                    <TableRowColumn>{row.id}</TableRowColumn>
+                    <TableRowColumn>{row.database}</TableRowColumn>
+                    <TableRowColumn>{row.sample_id}</TableRowColumn>
+                    <TableRowColumn>{row.investigation_type}</TableRowColumn>
+                    <TableRowColumn>{row.project_name}</TableRowColumn>
+                    <TableRowColumn>{row.lat_lon}</TableRowColumn>
+                    <TableRowColumn>{row.depth}</TableRowColumn>
+                    <TableRowColumn>{row.alt_elev}</TableRowColumn>
+                    <TableRowColumn>{row.geo_loc_name}</TableRowColumn>
+                    <TableRowColumn>{row.collection_date}</TableRowColumn>
+                    <TableRowColumn>{row.env_biome}</TableRowColumn>
+                    <TableRowColumn>{row.env_feature}</TableRowColumn>
+                    <TableRowColumn>{row.env_material}</TableRowColumn>
+                    <TableRowColumn>{row.env_package}</TableRowColumn>
+                    <TableRowColumn>{row.seq_meth}</TableRowColumn>
+                  </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+});
+
+export default ExploreTable;
