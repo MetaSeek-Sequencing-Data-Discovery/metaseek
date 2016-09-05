21ef299334d9c77cebe8827b912000cfed7fabf1
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-09-05 18:48:05 -0400

    Update welcome page styles / layout

--- scripts/components/Welcome.js
+++ scripts/components/Welcome.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router';
 
 // Material Design imports
-import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
 
 // My component imports
 import Header from './Header';
@@ -16,10 +17,19 @@ var Welcome = React.createClass({
     var styles = {
       container: {
         textAlign: 'center',
-        paddingTop: 125,
-        width: '70%',
+        paddingTop: 45,
+        width: '80%',
         margin: '0 auto',
-        maxWidth: '420px'
+        maxWidth: '800px',
+        minWidth: '500px'
+      },
+      paper: {
+        'backgroundColor':'#92CBC5',
+        'width':200,
+        'height':200,
+        'padding':15,
+        'textAlign':'left',
+        'margin':'0 auto'
       }
     };
     return (
@@ -27,16 +37,24 @@ var Welcome = React.createClass({
         <Header history={this.props.history}/>
         <MuiThemeProvider>
           <div style={styles.container}>
-            <h1>Material-UI</h1>
-            <Link to='/browse'>
-              <RaisedButton label="Browse Discoveries" fullWidth={true}/>
-            </Link>
-            <Link to='/explore'>
-              <RaisedButton label="Explore Datasets" fullWidth={true}/>
-            </Link>
-            <Link to='/dataset/new'>
-              <RaisedButton label="Contribute Data" fullWidth={true}/>
-            </Link>
+            <h1 style={{'fontSize':'2.6em'}}>Welcome to MetaSeek</h1>
+            <p style={{'marginTop':'50px','marginBottom':40}}>Discover, curate, and get access to thousands of sequencing samples from all over the web.</p>
+              <div style={{'display':'flex','maxWidth':500,'margin':'0 auto'}}>
+                <Paper style={styles.paper} zDepth={3}>
+                  <p style={{'margin':0}}>Explore publicly available samples by filtering, sorting, and saving your discoveries. Once you've curated a set of samples, easily download metadata and instructions for raw data download.</p>
+                  <Link style={{'display':'block','marginTop':15,'textAlign':'center'}} to='/explore'>
+                    <FlatButton label="DISCOVER"></FlatButton>
+                  </Link>
+                </Paper>
+                <Paper style={styles.paper} zDepth={3}>
+                  <p style={{'margin':0}}>Explore publicly available samples by filtering, sorting, and saving your discoveries. Once you've curated a set of samples, easily download metadata and instructions for raw data download.</p>
+                  <Link style={{'display':'block','marginTop':15,'textAlign':'center'}} to='/dataset/new'>
+                    <FlatButton label="CONTRIBUTE"></FlatButton>
+                  </Link>
+                </Paper>
+              </div>
+              <h1 style={{'fontSize':'1.8em','marginTop':60}}>What is MetaSeek?</h1>
+              <p style={{'marginTop':'50px'}}>MetaSEQ is a data discovery and analysis tool for genome sequencing data. Providing a rich front-end for exploration of metadata across a wide set of data repositories, use MetaSEQ to find the right aggregation of sequences for your analysis, and then access the raw sequencing data.</p>
           </div>
         </MuiThemeProvider>
       </div>
