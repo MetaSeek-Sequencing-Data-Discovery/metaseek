a0170e98c92752bb4b482d0f150b5c404debce2d
Nick Brown <nicholas.tyler.brown@gmail.com>
2017-03-11 15:44:35 -0600

    Tweak CORS settings
    all /api/ routes are available to all origins

--- server/app.py
+++ server/app.py
@@ -17,7 +17,7 @@ import json
 
 # Config / initialize the app, database and api
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/metaseek'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
