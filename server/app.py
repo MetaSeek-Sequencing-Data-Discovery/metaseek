ec379afcb056619910b15117d480884f019cda39
Nick Brown <nicholas.tyler.brown@gmail.com>
2017-03-11 17:01:18 -0600

    Cleaned up Dataset model definition, abstracted out API response fields for datasets into an object

--- server/app.py
+++ server/app.py
@@ -29,36 +29,40 @@ class Dataset(db.Model):
     # each attribute on a "Model" inherited class becomes a Column
     id = db.Column(db.Integer, primary_key=True)
 
+    biosample_link = db.Column(db.Text)
+    sample_title = db.Column(db.Text)
+    investigation_type = db.Column(db.Text)
+    library_source = db.Column(db.Text)
+    env_package = db.Column(db.Text)
+    collection_date = db.Column(db.DateTime)
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
-    #collection_date = db.Column(db.DateTime)
-    investigation_type = db.Column(db.Text)
-    env_package = db.Column(db.Text)
-    library_source = db.Column(db.Text)
+
+    seq_method = db.Column(db.Text)
     avg_read_length = db.Column(db.Float)
-    #seq_method = db.Column(db.Text)
     total_num_reads = db.Column(db.Integer)
     total_num_bases = db.Column(db.Integer)
     download_size = db.Column(db.Integer)
     avg_percent_gc = db.Column(db.Float)
-    biosample_link = db.Column(db.Text)
-    sample_title = db.Column(db.Text)
+
     #etc = db.Column(db.PickleType)
 
     # Each class must have an init function
-    def __init__(self, latitude=None, longitude=None, investigation_type=None, env_package=None, library_source=None, avg_read_length=None, total_num_reads=None, total_num_bases=None, download_size=None, avg_percent_gc=None, biosample_link=None, sample_title=None):
+    def __init__(self,biosample_link=None,sample_title=None,investigation_type=None,library_source=None,env_package=None,collection_date=None,latitude=None,longitude=None,seq_method=None,avg_read_length=None,total_num_reads=None,total_num_bases=None,download_size=None,avg_percent_gc=None):
         self.latitude = latitude
         self.longitude = longitude
         self.investigation_type = investigation_type
         self.env_package = env_package
         self.library_source = library_source
         self.avg_read_length = avg_read_length
+        self.collection_date = collection_date
         self.total_num_reads = total_num_reads
         self.total_num_bases = total_num_bases
         self.download_size = download_size
         self.avg_percent_gc = avg_percent_gc
         self.biosample_link = biosample_link
         self.sample_title = sample_title
+        self.seq_method = seq_method
 
     # Friendly string representation
     def __repr__(self):
@@ -207,11 +211,13 @@ class CreateDataset(Resource):
             parser.add_argument('avg_percent_gc',type=float)
             parser.add_argument('biosample_link',type=str)
             parser.add_argument('sample_title',type=str)
+            parser.add_argument('seq_method',type=str)
+            parser.add_argument('collection_date',type=str)
 
             args = parser.parse_args()
-            #datetimeguess = dateparser.parse(args['date'])
+            datetimeguess = dateparser.parse(args['collection_date'])
 
-            newDataset = Dataset(args['latitude'],args['longitude'],args['investigation_type'],args['env_package'], args['library_source'], args['avg_read_length'], args['total_num_reads'], args['total_num_bases'], args['download_size'], args['avg_percent_gc'], args['biosample_link'], args['sample_title'])
+            newDataset = Dataset(args['biosample_link'],args['sample_title'],args['investigation_type'],args['library_source'], args['env_package'],datetimeguess, args['latitude'], args['longitude'], args['seq_method'], args['avg_read_length'], args['total_num_reads'], args['total_num_bases'], args['download_size'],args['avg_percent_gc'])
             db.session.add(newDataset)
             db.session.commit()
             return {"dataset":{"uri":url_for('getdataset',id=newDataset.id,_external=True)}}
@@ -219,42 +225,32 @@ class CreateDataset(Resource):
         except Exception as e:
             return {'error': str(e)}
 
+marshalledDatasetFields = {
+    'latitude':fields.Float,
+    'longitude':fields.Float,
+    'investigation_type':fields.String,
+    'env_package':fields.String,
+    'library_source':fields.String,
+    'avg_read_length':fields.Float,
+    'total_num_reads':fields.Integer,
+    'total_num_bases':fields.Integer,
+    'download_size':fields.Integer,
+    'avg_percent_gc':fields.Float,
+    'biosample_link':fields.String,
+    'collection_date':fields.DateTime,
+    'seq_method':fields.String,
+    'sample_title':fields.String,
+    #this should also return the etc field for e.g. dataset details
+    'uri': fields.Url('getdataset', absolute=True)
+}
+
 class GetDataset(Resource):
-    @marshal_with({
-        'latitude':fields.Float,
-        'longitude':fields.Float,
-        'investigation_type':fields.String,
-        'env_package':fields.String,
-        'library_source':fields.String,
-        'avg_read_length':fields.Float,
-        'total_num_reads':fields.Integer,
-        'total_num_bases':fields.Integer,
-        'download_size':fields.Integer,
-        'avg_percent_gc':fields.Float,
-        'biosample_link':fields.String,
-        'sample_title':fields.String,
-        #this should also return the etc field for e.g. dataset details
-        'uri': fields.Url('getdataset', absolute=True)
-    }, envelope='dataset')
+    @marshal_with(marshalledDatasetFields, envelope='dataset')
     def get(self, id):
         return Dataset.query.get(id)
 
 class GetAllDatasets(Resource):
-    @marshal_with({
-        'latitude':fields.Float,
-        'longitude':fields.Float,
-        'investigation_type':fields.String,
-        'env_package':fields.String,
-        'library_source':fields.String,
-        'avg_read_length':fields.Float,
-        'total_num_reads':fields.Integer,
-        'total_num_bases':fields.Integer,
-        'download_size':fields.Integer,
-        'avg_percent_gc':fields.Float,
-        'biosample_link':fields.String,
-        'sample_title':fields.String,
-        'uri': fields.Url('getdataset', absolute=True)
-    }, envelope='datasets')
+    @marshal_with(marshalledDatasetFields, envelope='datasets')
     def get(self):
         return Dataset.query.all()
 
@@ -266,21 +262,7 @@ class GetDatasetSummary(Resource):
         return {"summary":{"totalDatasets":int(total),"totalDownloadSize":int(total_download_size)}}
 
 class SearchDatasets(Resource):
-    @marshal_with({
-        'latitude':fields.Float,
-        'longitude':fields.Float,
-        'investigation_type':fields.String,
-        'env_package':fields.String,
-        'library_source':fields.String,
-        'avg_read_length':fields.Float,
-        'total_num_reads':fields.Integer,
-        'total_num_bases':fields.Integer,
-        'download_size':fields.Integer,
-        'avg_percent_gc':fields.Float,
-        'biosample_link':fields.String,
-        'sample_title':fields.String
-        #'uri': fields.Url('getdataset', absolute=True)
-    }, envelope='datasets')
+    @marshal_with(marshalledDatasetFields, envelope='datasets')
     def post(self):
         try:
             parser = reqparse.RequestParser()
@@ -335,21 +317,7 @@ class GetDiscovery(Resource):
         'filter_params':fields.String,
         'timestamp':fields.DateTime(dt_format='rfc822'),
         'uri': fields.Url('getdiscovery', absolute=True),
-        'datasets':fields.Nested({
-            'latitude':fields.Float,
-            'longitude':fields.Float,
-            'investigation_type':fields.String,
-            'env_package':fields.String,
-            'library_source':fields.String,
-            'avg_read_length':fields.Float,
-            'total_num_reads':fields.Integer,
-            'total_num_bases':fields.Integer,
-            'download_size':fields.Integer,
-            'avg_percent_gc':fields.Float,
-            'biosample_link':fields.String,
-            'sample_title':fields.String,
-            'uri': fields.Url('getdataset', absolute=True)
-        }),
+        'datasets':fields.Nested(marshalledDatasetFields),
         'owner':fields.Nested({
             'firebase_id':fields.String,
             'uri':fields.Url('getuser', absolute=True)
@@ -363,21 +331,7 @@ class GetAllDiscoveries(Resource):
         'filter_params':fields.String,
         'timestamp':fields.DateTime(dt_format='rfc822'),
         'uri': fields.Url('getdiscovery', absolute=True),
-        'datasets':fields.Nested({
-            'latitude':fields.Float,
-            'longitude':fields.Float,
-            'investigation_type':fields.String,
-            'env_package':fields.String,
-            'library_source':fields.String,
-            'avg_read_length':fields.Float,
-            'total_num_reads':fields.Integer,
-            'total_num_bases':fields.Integer,
-            'download_size':fields.Integer,
-            'avg_percent_gc':fields.Float,
-            'biosample_link':fields.String,
-            'sample_title':fields.String,
-            'uri': fields.Url('getdataset', absolute=True)
-        }),
+        'datasets':fields.Nested(marshalledDatasetFields),
         'owner':fields.Nested({
             'firebase_id':fields.String,
             'uri':fields.Url('getuser', absolute=True)
