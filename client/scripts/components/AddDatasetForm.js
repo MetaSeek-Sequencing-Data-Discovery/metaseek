4a94f1ae273daaf3928041f6b114b441d21d3a2a
Nick Brown <nicholas.tyler.brown@gmail.com>
2017-03-11 17:00:48 -0600

    Add fields to form to match actual fields in the DB
    Does not yet push data to the server

--- client/scripts/components/AddDatasetForm.js
+++ client/scripts/components/AddDatasetForm.js
@@ -65,57 +65,67 @@ var AddDatasetForm = React.createClass({
            onInvalidSubmit={this.notifyFormError}
           >
           <Tabs>
-            <Tab label="Required" >
-               <FormsyText
-                 name="lat"
-                 validations="isNumeric"
-                 validationError={errorMessages.numericError}
-                 required
-                 hintText="Latitude of sample?"
-                 floatingLabelText="Latitude"
-               />
-               <FormsyText
-                 name="long"
-                 validations="isNumeric"
-                 validationError={errorMessages.numericError}
-                 required
-                 hintText="Longitude of sample?"
-                 floatingLabelText="Longitude"
-               />
-               <FormsyText
-               name="url"
-               validations="isUrl"
-               validationError={errorMessages.urlError}
+            <Tab label="Core Metadata" >
+              <FormsyText
+                name="sample_title"
+                validations="isWords"
+                validationError={errorMessages.wordsError}
+                required
+                hintText="Sample Title"
+                floatingLabelText="Sample Title"
+              />
+              <FormsyText
+              name="biosample_link"
+              validations="isUrl"
+              validationError={errorMessages.urlError}
+              required
+              hintText="http://www.example.com"
+              floatingLabelText="URL"
+              />
+              <FormsyText
+                name="investigation_type"
+                validations="isWords"
+                validationError={errorMessages.wordsError}
+                required
+                hintText="Investigation Type"
+                floatingLabelText="Investigation Type"
+              />
+              <FormsyText
+                name="library_source"
+                validations="isWords"
+                validationError={errorMessages.wordsError}
+                required
+                hintText="Library Source"
+                floatingLabelText="Library Source"
+              />
+              <FormsyText
+                name="env_package"
+                validations="isWords"
+                validationError={errorMessages.wordsError}
+                required
+                hintText="Env Package"
+                floatingLabelText="Env Package"
+              />
+              <FormsyDate
+                name="collection_date"
+                floatingLabelText="Collection Date"
+              />
+             <FormsyText
+               name="latitude"
+               validations="isNumeric"
+               validationError={errorMessages.numericError}
                required
-               hintText="http://www.example.com"
-               floatingLabelText="URL"
-               />
-
-               <FormsyDate
-                 name="date"
-                 floatingLabelText="Date"
-               />
-               <FormsySelect
-                 name="database"
-                 required
-                 floatingLabelText="Database?"
-                 hintText="Database of sample?"
-                 menuItems={this.selectFieldItems}
-               >
-                 <MenuItem value={'genbank'} primaryText="Genbank" />
-                 <MenuItem value={'edi'} primaryText="EDI" />
-                 <MenuItem value={'other'} primaryText="Other" />
-               </FormsySelect>
-               <FormsyText
-                 style={{'display':'block'}}
-                 name="description"
-                 validations="isWords"
-                 validationError={errorMessages.wordsError}
-                 hintText="Anything to add?"
-                 floatingLabelText="Description"
-                 multiLine={true}
-                 rows={4}
-               />
+               hintText="Latitude of sample?"
+               floatingLabelText="Latitude"
+             />
+             <FormsyText
+               name="longitude"
+               validations="isNumeric"
+               validationError={errorMessages.numericError}
+               required
+               hintText="Longitude of sample?"
+               floatingLabelText="Longitude"
+             />
                {/*<FormsyRadioGroup name="shipSpeed" defaultSelected="not_light">
                  <FormsyRadio
                    value="light"
@@ -135,22 +145,80 @@ var AddDatasetForm = React.createClass({
                  />
                </FormsyRadioGroup>*/}
               </Tab>
-              <Tab label="Optional" >
-                <div>
-                  <h2>Optional Metadata</h2>
-                  <p>
-                    This is another example tab.
-                  </p>
-                </div>
+              <Tab label="Read Details" >
+                {/*<FormsySelect
+                  name="database"
+                  required
+                  floatingLabelText="Database?"
+                  hintText="Database of sample?"
+                  menuItems={this.selectFieldItems}
+                >
+                  <MenuItem value={'genbank'} primaryText="Genbank" />
+                  <MenuItem value={'edi'} primaryText="EDI" />
+                  <MenuItem value={'other'} primaryText="Other" />
+                </FormsySelect>*/}
+
+                <FormsyText
+                  name="seq_method"
+                  validations="isWords"
+                  validationError={errorMessages.wordsError}
+                  required
+                  hintText="Sequencing Method"
+                  floatingLabelText="Sequencing Method"
+                />
+                <FormsyText
+                  name="avg_read_length"
+                  validations="isNumeric"
+                  validationError={errorMessages.numericError}
+                  required
+                  hintText="Average Read Length"
+                  floatingLabelText="Average Read Length"
+                />
+                <FormsyText
+                  name="total_num_reads"
+                  validations="isNumeric"
+                  validationError={errorMessages.numericError}
+                  required
+                  hintText="Total Number of Reads"
+                  floatingLabelText="Total Number of Reads"
+                />
+                <FormsyText
+                  name="total_num_bases"
+                  validations="isNumeric"
+                  validationError={errorMessages.numericError}
+                  required
+                  hintText="Total Number of Bases"
+                  floatingLabelText="Total Number of Bases"
+                />
+                <FormsyText
+                  name="download_size"
+                  validations="isNumeric"
+                  validationError={errorMessages.numericError}
+                  required
+                  hintText="Download Size (in MB)"
+                  floatingLabelText="Download Size (in MB)"
+                />
+                <FormsyText
+                  name="avg_percent_gc"
+                  validations="isNumeric"
+                  validationError={errorMessages.numericError}
+                  required
+                  hintText="Average Percent GC"
+                  floatingLabelText="Average Percent GC"
+                />
               </Tab>
-              <Tab label="Super Optional">
-                <div>
-                  <h2>Extremely Optional Metadata</h2>
-                  <p>
-                    Who would ever even come here?
-                  </p>
-                </div>
-              </Tab>
+              <Tab label="More Info" >
+              <FormsyText
+                style={{'display':'block'}}
+                name="description"
+                validations="isWords"
+                validationError={errorMessages.wordsError}
+                hintText="Anything to add?"
+                floatingLabelText="Description"
+                multiLine={true}
+                rows={4}
+              />
+            </Tab>
             </Tabs>
             <RaisedButton
               style={styles.submitStyle}
