2a5f0121ca367c70914324642d3df3615c982f9f
Nick Brown <nicholas.tyler.brown@gmail.com>
2017-03-19 21:22:24 -0400

    Switch to new firebase project (meta seek vs. metaseq)

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -4,9 +4,9 @@ import routes from './routes.js';
 
 import Firebase from 'firebase';
 var config = {
-    apiKey: "AIzaSyDH2OMMedUz3hF4dIRuDIdWpvUG3k00Eu8",
-    authDomain: "metaseq-6b779.firebaseapp.com",
-    databaseURL: "https://metaseq-6b779.firebaseio.com"
+    apiKey: "AIzaSyBD-VhWJEf-wOC_WDWGTM_moQqw0SzAY34",
+    authDomain: "metaseek-ba761.firebaseapp.com",
+    databaseURL: "https://metaseek-ba761.firebaseio.com"
   };
 Firebase.initializeApp(config);
 
