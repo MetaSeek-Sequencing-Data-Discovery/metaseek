ab66964b7d208a63cf25d38d7263ca667f99479e
Nick Brown <nicholas.tyler.brown@gmail.com>
2017-03-23 02:57:09 -0400

    Clean up sign-up form

--- client/scripts/components/Signup.js
+++ client/scripts/components/Signup.js
@@ -78,8 +78,9 @@ var Signup = React.createClass({
                  onValidSubmit={this.submitEmail}
                  onInvalidSubmit={this.notifyFormError}
                 >
-                  <p style={{'fontSize':'1.1em'}}>Enter your email address to be notified when our beta version is released:</p>
+                  <p style={{'textAlign':'center','fontSize':'1.1em'}}>Enter your email address to be notified when our beta version is released</p>
                   <FormsyText
+                  style={{'display':'block','margin':'0 auto'}}
                   name="email address"
                   validations="isEmail"
                   validationError={errorMessages.emailError}
@@ -111,34 +112,22 @@ var Signup = React.createClass({
                       minWidth: '500px'}}>
                 <h3>MetaSeek brings together sequencing metadata from all the major repositories to let you easily search, filter, and curate sequencing datasets for your meta-analysis.</h3>
               </Paper>
-
-              <div style={{'width':'600px','position':'absolute', 'left':'50%','top':'50%'}}>
-                <div id="architecture img" style={{'float':'left','width':'300px'}}>
-                  <img src="./images/architecture.jpeg"/>
-                </div>
-                <div id="image descriptor" style={{'float':'right','width':'200px','textSize':'1.3em','padding':25}}>
-                  <Paper>
-                    <p>Metadata is scraped from individual sequencing repositories, unifying field names, complying to MIxS Genomic Standards Consortium metadata standards, and parsing manually-entered values like 'latitude' and 'longitude' to machine-readable format wherever possible.</p>
-                  </Paper>
-                  <Paper>
-                    <p>MetaSeek sends data to the browser in an intelligent way, allowing you to filter, search, and visualize results from the MetaSeek database almost instantaneously.</p>
-                  </Paper>
-                </div>
-              </div>
               <h1 style={{'fontSize':'1.9em','marginTop':60}}>How to use MetaSeek</h1>
-              <h2>Explore Datasets</h2>
-              <p>Search and filter on the metadata you care about</p>
-              <div>
-                <img src="./images/explore.jpeg"/>
-              </div>
-              <h2>Save and share your discovery</h2>
-              <p>Get dummy code for data download.</p>
-              <p>Browse other users’ discoveries, save ones you like or use one as a launching off point.</p>
-              <h2>Contribute to the metadata pool</h2>
-              <p>Metadata is only as good as the community it serves.</p>
-              <p>Flag or correct metadata errors, or annotate a dataset or discovery.</p>
-
-
+              <Paper style={{width:'1000px','margin':'12px auto'}} zDepth={2}>
+                <div style={{float:'left',width:500,'marginLeft':-120}}>
+                  <img src="./images/explore.jpeg"/>
+                </div>
+                <div style={{float:'right',width:500}}>
+                  <h2>Explore Datasets</h2>
+                    <p>Search and filter on the metadata you care about</p>
+                  <h2>Save and share your discovery</h2>
+                    <p>Get dummy code for data download.</p>
+                    <p>Browse other users’ discoveries, save ones you like or use one as a launching off point.</p>
+                  <h2>Contribute to the metadata pool</h2>
+                    <p>Metadata is only as good as the community it serves.</p>
+                    <p>Flag or correct metadata errors, or annotate a dataset or discovery.</p>
+                </div>
+              </Paper>
           </div>
       </MuiThemeProvider>
     </div>
