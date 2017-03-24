7001909874bfd5706b2c9586faaa0a3bbf45e753
Nick Brown <nicholas.tyler.brown@gmail.com>
2017-03-24 01:45:10 -0400

    More signup cleanup, save emails to database

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './routes.js';
-import firebaseConfig from './config/firebase.js';
-
-import Firebase from 'firebase';
-
-Firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(routes, document.querySelector('#main'))
