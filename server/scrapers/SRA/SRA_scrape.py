ca0ab178e10a019e452f631d144d7364d549517a
ahoarfrost <adrienne.l.hoarfrost@gmail.com>
2017-06-27 14:26:04 -0400

    subtract uids already in metaseek db from uid_list when scraping SRA

--- server/scrapers/SRA/SRA_scrape.py
+++ server/scrapers/SRA/SRA_scrape.py
@@ -1,4 +1,97 @@
 from SRA_scrape_fns import *
+from app import db
+from models import *
+from datetime import datetime
+
+metaseek_fields = ['db_source_uid',
+'db_source',
+'expt_link',
+'expt_id',
+'expt_title',
+'expt_design_description',
+'library_name',
+'library_strategy',
+'library_source',
+'library_screening_strategy',
+'library_construction_method',
+'library_construction_protocol',
+'sequencing_method',
+'instrument_model',
+'submission_id',
+'organization_name',
+'organization_address',
+'organization_contacts',
+'study_id',
+'bioproject_id',
+'study_title',
+'study_type',
+'study_type_other',
+'study_abstract',
+'study_links',
+'study_attributes',
+'sample_id',
+'biosample_id',
+'sample_title',
+'ncbi_taxon_id',
+'taxon_scientific_name',
+'taxon_common_name',
+'sample_description',
+'num_runs_in_accession',
+'run_ids_maxrun',
+'library_reads_sequenced_maxrun',
+'total_num_bases_maxrun',
+'download_size_maxrun',
+'avg_read_length_maxrun',
+'baseA_count_maxrun',
+'baseC_count_maxrun',
+'baseG_count_maxrun',
+'baseT_count_maxrun',
+'baseN_count_maxrun',
+'gc_percent_maxrun',
+'run_quality_counts_maxrun',
+'biosample_uid',
+'nuccore_uids',
+'biosample_link',
+'metadata_publication_date',
+'biosample_package',
+'biosample_models',
+'sample_attributes',
+'investigation_type',
+'env_package',
+'project_name',
+'lat_lon',
+'latitude',
+'longitude',
+'geo_loc_name',
+'collection_date',
+'collection_time',
+'env_biome',
+'env_feature',
+'env_material',
+'depth',
+'elevation',
+'altitude',
+'target_gene',
+'target_subfragment',
+'ploidy',
+'num_replicons',
+'estimated_size',
+'ref_biomaterial',
+'propagation',
+'assembly',
+'finishing_strategy',
+'isol_growth_condt',
+'experimental_factor',
+'specific_host',
+'subspecific_genetic_lineage',
+'tissue',
+'sex',
+'sample_type',
+'age',
+'dev_stage',
+'biomaterial_provider',
+'host_disease',
+'date_scraped']
 
 if __name__ == "__main__":
     #check if error log file exists; if it doesn't, create one
@@ -15,15 +108,21 @@ if __name__ == "__main__":
     uid_list = get_uid_list(ret_list=retstart_list)
 
     #remove SRA IDs that have already been ingested into MetaSeek DB
+    #find existing db_source_uids
+    result = db.session.query(Dataset.db_source_uid).filter(Dataset.db_source=='SRA').distinct()
+    existing_uids = [r.db_source_uid for r in result]
+    #subtract any uids already in db from uid_list
+    uids_to_scrape = set(uid_list)-set(existing_uids)
+
     #source_db_uid for which 'source_db' field = 'SRA'
     ##TODO: figure out how to open db session in here?
 
     #split UIDs to scrape into batches of 500 (max number of UIDs can call with eutilities api at one time)
-    batches = get_batches(uid_list)
+    batches = get_batches(uids_to_scrape)
     #for each batch of 500 UIDs, scrape metadata
     for batch_ix,batch in enumerate(batches):
         print "PROCESSING BATCH %s OUT OF %s......" % (batch_ix+1,len(batches))
-        batch_uid_list = map(int,uid_list[batch[0]:batch[1]])
+        batch_uid_list = map(int,uids_to_scrape[batch[0]:batch[1]])
         print "-%s UIDs to scrape in this batch...... %s..." % (len(batch_uid_list),batch_uid_list[0:10])
         #scrape sra metadata, return as dictionary of dictionaries; each sdict key is the SRA UID, value is a dictionary of srx metadata key/value pairs
         print "-scraping SRX metadata..."
