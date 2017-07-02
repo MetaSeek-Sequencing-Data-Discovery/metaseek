5139db359a9b2035c4a5ff45c8591216691278da
Nick Brown <uptownnickbrown@users.noreply.github.com>
2017-07-01 23:46:36 -0400

    Fix a couple issues
    * when there is only one value for a binned rollup
    * when you try to log bin a column with negative numbers
    * when the min value is very close to the rangeMin for bin calc

--- server/helpers.py
+++ server/helpers.py
@@ -36,11 +36,12 @@ def filterQueryByRule(targetClass,queryObject,field,ruletype,value):
 
 def summarizeColumn(dataFrame,columnName,linearBins=False,logBins=False):
     dataColumn = dataFrame[columnName].dropna()
-    if len(dataColumn.unique()) == 0:
+    uniqueCount = len(dataColumn.unique())
+    if uniqueCount == 0:
         return {'NULL':len(dataFrame.index)}
     else:
         if logBins == False:
-            if linearBins == False or len(dataColumn)==1:
+            if linearBins == False or uniqueCount == 1:
                 groupedColumn = dataColumn.groupby(dataColumn)
                 countedColumn = groupedColumn.size()
                 countedColumnDict = dict(countedColumn)
@@ -48,11 +49,18 @@ def summarizeColumn(dataFrame,columnName,linearBins=False,logBins=False):
             else: #linear bin hist
                 minValue = np.amin(dataColumn)
                 maxValue = np.amax(dataColumn)
-                roundTo = int(round(np.log10(maxValue-minValue)) * -1) + 1
 
+                roundTo = int(round(np.log10(maxValue-minValue)) * -1) + 1
                 binSize = 10**(-1 * roundTo)
-                rangeMin = round(np.amin(dataColumn),roundTo)
-                rangeMax = round(np.amax(dataColumn),roundTo) + binSize * 2
+
+                # protect against weird rounding issues by expanding range if needed
+                rangeMin = round(math.floor(minValue),roundTo)
+                if (minValue < rangeMin):
+                    rangeMin = rangeMin - binSize
+
+                rangeMax = round(math.ceil(maxValue),roundTo) + binSize
+                if (maxValue > rangeMax):
+                    rangeMax = rangeMax + binSize
 
                 if binSize >= 1:
                     binSize = int(binSize)
@@ -68,29 +76,35 @@ def summarizeColumn(dataFrame,columnName,linearBins=False,logBins=False):
                 for index, count in enumerate(counts):
                     # histogram labels should be ranges, not values
                     # or we need to store min / max bounds for each bin and pass that to the frontend somehow
-                    histogramBinString = str(binEdges[index]) + '-' + str(binEdges[index + 1])
+                    histogramBinString = str(binEdges[index]) + ' - ' + str(binEdges[index + 1])
                     roundedCounts[histogramBinString] = count
                 return roundedCounts
         else: #logbin hist
-            minValue = np.amin(dataColumn)
-            minPowerFloor = math.floor(np.log10(minValue))
-            maxValue = np.amax(dataColumn)
-            maxPowerCeil = math.ceil(np.log10(maxValue))
+            dataColumn = dataColumn[dataColumn > 0]
+            if len(dataColumn) == 0:
+                return {}
+            else:
+                minValue = np.amin(dataColumn)
+                minPowerFloor = math.floor(np.log10(minValue))
+                maxValue = np.amax(dataColumn)
+                maxPowerCeil = math.ceil(np.log10(maxValue))
 
-            numBins = maxPowerCeil - minPowerFloor + 1
-            logBins = np.logspace(minPowerFloor,maxPowerCeil,num=numBins)
+                numBins = maxPowerCeil - minPowerFloor + 1
+                if minPowerFloor == maxPowerCeil:
+                    maxPowerCeil = maxPowerCeil + 1
+                logBins = np.logspace(minPowerFloor,maxPowerCeil,num=numBins)
 
-            counts, binEdges = np.histogram(dataColumn,bins=logBins)
+                counts, binEdges = np.histogram(dataColumn,bins=logBins)
 
-            logBinnedCounts = {}
-            for index, count in enumerate(counts):
-                # histogram labels should be ranges, not values
-                # or we need to store min / max bounds for each bin and pass that to the frontend somehow
-                histogramBinString = '10^' + str(int(index + minPowerFloor)) + '-' + '10^' + str(int(index + 1 + minPowerFloor))
-                # not sure whether I like the scientific notation strings more than this or not:
-                # histogramBinString = str(int(binEdges[index])) + '-' + str(int(binEdges[index + 1]))
-                logBinnedCounts[histogramBinString] = count
-            return logBinnedCounts
+                logBinnedCounts = {}
+                for index, count in enumerate(counts):
+                    # histogram labels should be ranges, not values
+                    # or we need to store min / max bounds for each bin and pass that to the frontend somehow
+                    histogramBinString = '10^' + str(int(index + minPowerFloor)) + ' - ' + '10^' + str(int(index + 1 + minPowerFloor))
+                    # not sure whether I like the scientific notation strings more than this or not:
+                    # histogramBinString = str(int(binEdges[index])) + '-' + str(int(binEdges[index + 1]))
+                    logBinnedCounts[histogramBinString] = count
+                return logBinnedCounts
 
 def summarizeDatasets(queryObject):
     queryResultDataframe = pd.read_sql(queryObject.statement,db.session.bind)
