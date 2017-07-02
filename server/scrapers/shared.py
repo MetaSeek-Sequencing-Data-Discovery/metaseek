af2123ad3d3c7d372f83027e660fe54ed6296e68
Nick Brown <uptownnickbrown@users.noreply.github.com>
2017-07-01 22:26:18 -0400

    Remove unneeded complex lat / lon parsing

--- server/scrapers/shared.py
+++ server/scrapers/shared.py
@@ -2,42 +2,15 @@ import re
 
 def parseLatLon(lat_lon_string):
 
-    pattern1 = re.compile(r'[N,S][-+]?\d*\.\d+ [W,E][-+]?\d*\.\d+|[N,S][-+]?\d+ [W,E][-+]?\d+')
     pattern2 = re.compile(r"[-+]?\d*\.\d+ [N,S] [-+]?\d*\.\d+ [W,E]|\d+ [N,S] \d+ [W,E]|\d+ [N,S] [-+]?\d*\.\d+ [W,E]|[-+]?\d*\.\d+ [N,S] \d+ [W,E]")
     pattern3 = re.compile(r'[-+]?\d*\.\d+ [-+]?\d*\.\d+|[-+]?\d*\.\d+ [-+]?\d+')
     pattern4 = re.compile(r'[-+]?\d*\.\d+, [-+]?\d*\.\d+')
-    pattern5 = re.compile(r"[-+]?\d*\.\d+ DD [-+]?\d*\.\d+ DD|[-+]?\d+ DD [-+]?\d+ DD|[-+]?\d+ DD [-+]?\d*\.\d+ DD|[-+]?\d*\.\d+ DD [-+]?\d+ DD")
-    pattern6 = re.compile(r"[-+]?\d*\.\d+ [N,S], [-+]?\d*\.\d+ [W,E]|\d+ [N,S], \d+ [W,E]|\d+ [N,S], [-+]?\d*\.\d+ [W,E]|[-+]?\d*\.\d+ [N,S], \d+ [W,E]")
-    pattern7 = re.compile(r'[-+]?\d*\.\d+ - [-+]?\d*\.\d+')
-    pattern8 = re.compile(r'\([N,S]\:[W,E]\) [-+]?\d*\.\d+\:[-+]?\d*\.\d+')
-    pattern9 = re.compile(r'[-+]?\d+\'\d+ [N,S] [-+]?\d+\'\d+ [W,E]')
-    pattern10 = re.compile(r"[-+]?\d*\.\d+ [E,W], [-+]?\d*\.\d+ [N,S]")
-    pattern11 = re.compile(r'[-+]?\d+\xc2\xb0 \d*\.\d+\'[N,S,E,W]')
 
-    match1 = re.findall(pattern1,   lat_lon_string)
     match2 = re.findall(pattern2,   lat_lon_string)
     match3 = re.findall(pattern3,   lat_lon_string)
     match4 = re.findall(pattern4,   lat_lon_string)
-    match5 = re.findall(pattern5,   lat_lon_string)
-    match6 = re.findall(pattern6,   lat_lon_string)
-    match7 = re.findall(pattern7,   lat_lon_string)
-    match8 = re.findall(pattern8,   lat_lon_string)
-    match9 = re.findall(pattern9,   lat_lon_string)
-    match10 = re.findall(pattern10, lat_lon_string)
-    match11 = re.findall(pattern11, lat_lon_string)
 
-    if len(match1)>0:
-        print 'matched 1'
-        split2 = match1[0].split(' ')
-        lat = float(split2[0][1:])
-        lon = float(split2[1][1:])
-        if split2[0][0]=='S':
-            lat = lat*-1
-        if split2[1][0]=='W':
-            lon = lon*-1
-        return lat, lon
-
-    elif len(match2)>0:
+    if len(match2)>0:
         print 'matched 2'
         split = [x.split(' ') for x in match2]
         lat = split[0][0]
@@ -70,96 +43,6 @@ def parseLatLon(lat_lon_string):
         split4 = match4[0].split(', ')
         lat = float(split4[0])
         lon = float(split4[1])
-        return lat, lon
-
-    elif len(match5)>0:
-        print 'matched 5'
-        split = [x.split(' ') for x in match5]
-        lat = float(split[0][0])
-        lon = float(split[0][2])
-        return lat, lon
-
-    elif len(match6)>0:
-        print 'matched 6'
-        split = [x.split(', ') for x in match6]
-        split_lat = split[0][0].split(' ')
-        split_lon = split[0][1].split(' ')
-        lat = float(split_lat[0])
-        lon = float(split_lon[0])
-        if split_lat[1]=='S':
-            lat = float(lat)*-1
-        if split_lon[1]=='W':
-            lon = float(lon)*-1
-        return lat, lon
-
-    elif len(match7)>0:
-        print 'matched 7'
-        split = match7[0].split(' - ')
-        lat = float(split[0])
-        lon = float(split[1])
-        if lat>1000:
-            lat = lat/10000.0 #e.g. -479595 = 47.7027
-        if lon>1000:
-            lon = lon/10000.0
-        return lat, lon
-
-    elif len(match8)>0:
-        print 'matched 8'
-        hems = match8[0].split(' ')[0][1:-1]
-        hems = hems.split(':')
-        nums = match8[0].split(' ')[1].split(':')
-        lat = float(nums[0])
-        lon = float(nums[1])
-        if hems[0]=='S':
-            lat = lat*-1
-        if hems[1]=='W':
-            lon = lon*-1
-        return lat, lon
-
-    elif len(match9)>0:
-        print 'matched 9'
-        split = match9[0].split(' ')
-        lat = split[0]
-        lat = re.sub('\'', '', lat)
-        lat = float(lat)/10000
-        if split[1]=='S':
-            lat = lat*-1
-        lon = split[2]
-        lon = re.sub('\'', '', lon)
-        lon = float(lon)/10000
-        if split[3]=='W':
-            lon = lon*-1
-        return lat, lon
-
-    elif len(match10)>0:
-        print 'matched 10'
-        split = [x.split(', ') for x in match10]
-        split_lat = split[0][0].split(' ')
-        split_lon = split[0][1].split(' ')
-        lat = float(split_lat[0])
-        lon = float(split_lon[0])
-        if split_lat[1]=='S':
-            lat = float(lat)*-1
-        if split_lon[1]=='W':
-            lon = float(lon)*-1
-        return lat, lon
-
-    elif len(match11)>0:
-        print 'matched 11'
-        lat = match11[0]
-        lat = re.sub('\xc2\xb0 ', '', lat)
-        lat = re.sub('\.','',lat)
-        lat = lat.split('\'')
-        lat = float(lat[0])/10000
-        if lat[1]=='S':
-            lat = lat*-1
-        lon = match12[1]
-        lon = re.sub('\xc2\xb0 ', '', lon)
-        lon = re.sub('\.','',lon)
-        lon = lon.split('\'')
-        lon = float(lon[0])/10000
-        if lon[1]=='W':
-            lon = lon*-1
         return lat, lon
 
     else:
