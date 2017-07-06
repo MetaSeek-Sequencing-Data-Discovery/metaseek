25ff2396aa821ff88f32a87d255c4e1f1c7b4fde
Nick Brown <uptownnickbrown@users.noreply.github.com>
2017-07-05 21:31:28 -0400

    Fix a CORS issue
    Just open it up for all resources / all origins

--- server/app.py
+++ server/app.py
@@ -14,7 +14,7 @@ dbPass = os.environ['METASEEK_DB']
 
 # Config / initialize the app, database and api
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
 
 # production DB
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://metaseek:' + dbPass + '@ec2-35-166-20-248.us-west-2.compute.amazonaws.com/metaseek'
