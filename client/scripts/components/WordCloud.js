f7ec5e2149802a4934e8247da0a31f39d3f7e30a
ahoarfrost <adrienne.l.hoarfrost@gmail.com>
2017-07-08 22:45:16 -0700

    add onClick event to word cloud

--- client/scripts/components/WordCloud.js
+++ client/scripts/components/WordCloud.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { TagCloud } from 'react-tagcloud';
 import {randomColor} from 'randomcolor';
 
+
 var WordCloud = React.createClass({
+  getInitialState : function() {
+    return {
+      'hoverValue':{value:'click on word to see count'},
+    }
+  },
+
+  showHover : function(value, event) {
+    this.setState({"hoverValue":value});
+  },
 
   render : function() {
     var colors = ["#6369E0","#8387E6","#9396E9","#A3A5EC","#C3C3F2","#FFD3C8","#FFCBBE","#FC99E8","#FCADEC","#FCC1F0","#82FFEA","#A9FFF0","#C3FFF4","#FFB3A0","#B1FEF1"]
@@ -17,10 +27,15 @@ var WordCloud = React.createClass({
       });
 
     return(
-      <TagCloud tags={clouddata}
-      minSize={16}
-      maxSize={48}
-      />
+      <div>
+        <span>{this.state.hoverValue.value} : {this.state.hoverValue.count}</span>
+        <TagCloud tags={clouddata}
+        minSize={16}
+        maxSize={48}
+        onClick={this.showHover}
+        shuffle={false}
+        />
+      </div>
     )}
   });
 
