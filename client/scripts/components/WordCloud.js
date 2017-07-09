de338c02b79b5954d0d28c5478e3ecba0d2c46df
Nick Brown <uptownnickbrown@users.noreply.github.com>
2017-07-09 19:28:44 -0400

    Clean up / comment / make the Word Cloud prettier

--- client/scripts/components/WordCloud.js
+++ client/scripts/components/WordCloud.js
@@ -3,9 +3,7 @@ import { TagCloud } from 'react-tagcloud';
 
 var WordCloud = React.createClass({
   getInitialState : function() {
-    return {
-      'hoverValue':{value:'click on word to see count'},
-    }
+    return {}
   },
 
   showHover : function(value, event) {
@@ -13,25 +11,60 @@ var WordCloud = React.createClass({
   },
 
   render : function() {
-    var colors = ["#6369E0","#8387E6","#9396E9","#A3A5EC","#C3C3F2","#FFD3C8","#FFCBBE","#FC99E8","#FCADEC","#FCC1F0","#82FFEA","#A9FFF0","#C3FFF4","#FFB3A0","#B1FEF1"]
-    var data = this.props.activeSummaryData[this.props.wordinput];
-    var clouddata = Object.keys(data).filter(function(value, index) {
-      if (value === "no data" | value === "other categories") {
-        return false; // skip
+    // Colors from color brewer http://colorbrewer2.org/#type=sequential&scheme=PuBuGn&n=8
+    // Order matters - higher values will get the later colors in this list
+    // Another good option http://colorbrewer2.org/?type=sequential&scheme=BuGn&n=7
+    var colors = ['#d0d1e6','#a6bddb','#67a9cf','#3690c0','#02818a','#016450'];
+
+    // Min / max font sizes in pixels for words in word cloud
+    var min = 8;
+    var max = 40; // has to be pretty small to fit in the word cloud box
+
+    // this.props.wordinput is a field name that can be changed by the user
+    // pull the right summary data from activeSummaryData
+    var activeFieldData = this.props.activeSummaryData[this.props.wordinput];
+
+    // remove nulls / boring values
+    var activeFieldDataValidKeys = Object.keys(activeFieldData).filter(
+      function(value, index) {
+        if (value == "no data" || value == "other categories") {
+          return false; // skip
+        } else {
+          return true;
+        }
       }
-      return true;}).map(function(value,index) {
-        var rand = colors[Math.floor(Math.random() * colors.length)]
-        return {"value":value,"count":data[value],"color":rand};
-      });
+    );
+
+    // Format data the way TagCloud wants it
+    var wordCloudData = activeFieldDataValidKeys.map(
+      function(value,index) {
+        var count = activeFieldData[value];
+        return {"value":value,"count":count};
+      }
+    );
+
+    // Use a customRenderer so we can make the color scale with the value vs. be random
+    var customRenderer = function(tag, size) {
+      // Find how far in between min and max the tag size is, find the right color for that slot
+      var colorScaler = ((size - min) / (max - min)) * (colors.length - 1);
+      var color = colors[Math.floor(colorScaler)];
+
+      // More styles in css for word-cloud-tag - only on-the-fly calculated styles go here
+      return <span key={'word-' + tag.value}
+            className='word-cloud-tag'
+            style={{
+              fontSize: size + 'px',
+              color: color
+            }}>{tag.value}</span>
+    };
 
     return(
       <div>
-        <span>{this.state.hoverValue.value} : {this.state.hoverValue.count}</span>
-        <TagCloud tags={clouddata}
-        minSize={16}
-        maxSize={48}
-        onClick={this.showHover}
-        shuffle={false}
+        <TagCloud
+          tags={wordCloudData}
+          minSize={min}
+          maxSize={max}
+          renderer={customRenderer}
         />
       </div>
     )}
