8a6b672bcd901cb7f48f31f30f4f943360f3f4f0
ahoarfrost <adrienne.l.hoarfrost@gmail.com>
2017-09-03 00:22:53 -0400

    add hash scrolls

--- client/scripts/components/Glossary.js
+++ client/scripts/components/Glossary.js
@@ -3,6 +3,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import ColorPalette from './ColorPalette';
 import Paper from 'material-ui/Paper';
+import ScrollableAnchor from 'react-scrollable-anchor';
 
 var Glossary = React.createClass ({
   getInitialState : function() {
@@ -20,69 +21,101 @@ var Glossary = React.createClass ({
           <div>
             <h2>Glossary</h2>
             <h3 className="glossary-term">Average Read Length</h3>
-            <section className="glossary-item" id="avg_read_length">
-              {glossary["avg_read_length"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="avg_read_length">
+                {glossary["avg_read_length"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Environmental Biome</h3>
-            <section className="glossary-item" id="env_biome">
-              {glossary["env_biome"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="env_biome">
+                {glossary["env_biome"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Environmental Feature</h3>
-            <section className="glossary-item" id="env_feature">
-              {glossary["env_feature"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="env_feature">
+                {glossary["env_feature"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Environmental Material</h3>
-            <section className="glossary-item" id="env_material">
-              {glossary["env_material"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="env_material">
+                {glossary["env_material"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Environmental Package</h3>
-            <section className="glossary-item" id="env_package">
-              {glossary["env_package"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="env_package">
+                {glossary["env_package"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Geographic Location</h3>
-            <section className="glossary-item" id="geo_loc_name">
-              {glossary["geo_loc_name"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="geo_loc_name">
+                {glossary["geo_loc_name"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Instrument Model</h3>
-            <section className="glossary-item" id="instrument_model">
-              {glossary["instrument_model"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="instrument_model">
+                {glossary["instrument_model"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Investigation Type</h3>
-            <section className="glossary-item" id="investigation_type">
-              {glossary["investigation_type"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="investigation_type">
+                {glossary["investigation_type"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Library Construction Method</h3>
-            <section className="glossary-item" id="library_construction_method">
-              {glossary["library_construction_method"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="library_construction_method">
+                {glossary["library_construction_method"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Library Reads Sequenced (Number of Reads Sequenced)</h3>
-            <section className="glossary-item" id="library_reads_sequenced">
-              {glossary["library_reads_sequenced"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="library_reads_sequenced">
+                {glossary["library_reads_sequenced"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Library Screening Strategy</h3>
-            <section className="glossary-item" id="library_screening_strategy">
-              {glossary["library_screening_strategy"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="library_screening_strategy">
+                {glossary["library_screening_strategy"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Library Source</h3>
-            <section className="glossary-item" id="library_source">
-              {glossary["library_source"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="library_source">
+                {glossary["library_source"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Library Strategy</h3>
-            <section className="glossary-item" id="library_strategy">
-              {glossary["library_strategy"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="library_strategy">
+                {glossary["library_strategy"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Percent GC</h3>
-            <section className="glossary-item" id="percent_gc">
-              {glossary["percent_gc"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="percent_gc">
+                {glossary["percent_gc"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Sequencing Method</h3>
-            <section className="glossary-item" id="sequencing_method">
-              {glossary["sequencing_method"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="sequencing_method">
+                {glossary["sequencing_method"]}
+              </section>
+            </ScrollableAnchor>
             <h3 className="glossary-term">Study Type</h3>
-            <section className="glossary-item" id="study_type">
-              {glossary["study_type"]}
-            </section>
+            <ScrollableAnchor>
+              <section className="glossary-item" id="study_type">
+                {glossary["study_type"]}
+              </section>
+            </ScrollableAnchor>
           </div>
         </Paper>
     </MuiThemeProvider>
