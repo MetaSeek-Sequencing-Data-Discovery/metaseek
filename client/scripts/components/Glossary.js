bf61978ca42bd7084ab42cad72f2b427c61a27d3
ahoarfrost <adrienne.l.hoarfrost@gmail.com>
2017-09-07 13:59:14 -0400

    add overview info to Glossary

--- client/scripts/components/Glossary.js
+++ client/scripts/components/Glossary.js
@@ -4,6 +4,7 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import ColorPalette from './ColorPalette';
 import Paper from 'material-ui/Paper';
 import ScrollableAnchor from 'react-scrollable-anchor';
+import { Link } from 'react-router';
 
 var Glossary = React.createClass ({
   getInitialState : function() {
@@ -20,6 +21,10 @@ var Glossary = React.createClass ({
         <Paper className="glossary-paper">
           <div>
             <h2>Glossary</h2>
+            <h3 className="glossary-term">Overview and Useful Links</h3>
+            <section className="glossary-item">
+              MetaSeek uses the MIxS metadata standards for sequencing samples. The Sequence Read Archive (SRA) makes it possible to submit a MIxS-compliant sample, but doesn't require it. The SRA also has its own set of mandatory fields that are usually very informative but not included in the MIxS standards. The SRA fields are defined in <Link to="https://www.ncbi.nlm.nih.gov/books/NBK54984/" target="_blank">their glossary</Link>. To see the BioSample sample attributes that are required for each type of package, see <Link to="https://submit.ncbi.nlm.nih.gov/biosample/template/" target="_blank">here</Link>. Many of these fields are controlled vocabularies, meaning that the data entry is supposed to be one of a fixed set of values. We find <Link to="http://www.ebi.ac.uk/ena/submit/preparing-xmls" target="_blank">this Preparing SRA submissions guide</Link> useful for a list of SRA controlled vocabulary fields for each metadata field, if it exists. <Link to="http://www.ebi.ac.uk/ena/submit/mixs-checklists#environment_specific" target="_blank">This page</Link> provides a nice summary of the mandatory fields for each type of MIxS-compliant sample. For definitions and controlled vocabulary values for all of the MIxS metadata fields, see <Link to="https://terms.tdwg.org/wiki/MIxS" target="_blank">here</Link>.
+            </section>
             <h3 className="glossary-term">Average Read Length</h3>
             <ScrollableAnchor>
               <section className="glossary-item" id="avg_read_length">
