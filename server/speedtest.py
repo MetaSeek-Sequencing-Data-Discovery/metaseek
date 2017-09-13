d43b3307ffa95e287d2194bc0c96b344df4e5527
Nick Brown <uptownnickbrown@users.noreply.github.com>
2017-09-12 23:15:41 -0400

    Test faster queries
    Hist binning with CASE
    RAND() sampling
    Skip SQLAlchemy, use mysql-connector
    
    Timing for various WHERE’s

---
+++ server/speedtest.py
@@ -0,0 +1,47 @@
+import time
+import mysql.connector
+
+# prod
+# cnx = mysql.connector.connect(user='nick', password='REDACTED',host='ec2-52-33-134-115.us-west-2.compute.amazonaws.com',database='metaseek')
+
+# local
+cnx = mysql.connector.connect(user='root',host='127.0.0.1',database='metaseek')
+
+cursor = cnx.cursor()
+
+subquery = "WHERE download_size_maxrun IS NOT NULL"
+
+query = ("""SELECT t.maxrun_range, count(*)
+AS count
+FROM (
+	SELECT CASE
+		WHEN download_size_maxrun BETWEEN 0 AND 33 THEN '0-33'
+		WHEN download_size_maxrun between 33 and 333 then '33-333'
+		WHEN download_size_maxrun BETWEEN 333 AND 3333 THEN '333-3333'
+		WHEN download_size_maxrun between 3333 and 33333 then '3333-33333'
+		WHEN download_size_maxrun BETWEEN 33333 AND 333333 THEN '33333-333333'
+		WHEN download_size_maxrun between 333333 and 3333333 then '333333-3333333'
+		WHEN download_size_maxrun between 3333333 and 33333333 then '3333333-33333333'
+		WHEN download_size_maxrun between 33333333 and 333333333 then '33333333-333333333'
+		WHEN download_size_maxrun between 333333333 and 3333333333 then '333333333-3333333333'
+		else '>3333333333'
+	END as maxrun_range
+FROM dataset """ + subquery + """ AND RAND() < 0.05) t
+GROUP BY t.maxrun_range""")
+
+def testQuery(query):
+    start = time.time()
+    print 'started at ' + str(start)
+    cursor.execute(query)
+    print '============ RESULTS ============== '
+    for row in cursor.fetchall():
+        print row
+    print '============ END ============== '
+    finish = time.time()
+    print 'finished at ' + str(finish)
+    print 'time to run ' + str(finish-start)
+
+testQuery(query)
+
+cursor.close()
+cnx.close()
