a77c884bd4aace526d96b2ed761a4b699d528add
ahoarfrost <adrienne.l.hoarfrost@gmail.com>
2017-10-07 18:09:24 -0400

    add link to beta tester survey

--- client/scripts/components/Header.js
+++ client/scripts/components/Header.js
@@ -11,6 +11,7 @@ import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 /*</ToolbarGroup>
@@ -42,6 +43,11 @@ var Header = React.createClass({
           </Link>
         </ToolbarGroup>
         <ToolbarGroup >
+          <RaisedButton
+            label="send feedback!"
+            primary={true}
+            href="https://www.surveymonkey.com/r/HDT27Y8"
+          />
           <div>
              <img className="header-image-beta" src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Beta-badge.svg"/>
           </div>
